3a3bc75fd2ad14ff83efa99f230f77498c111f7e
KH <ganghe74@gmail.com>
2022-02-20 22:56:29 +0900

    README.md

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-solve-ps
+# solve
+
+a small cli tool for ps
+
+---
+
+## install
+
+```bash
+git clone https://github.com/ganghe74/solve
+cd solve
+pip install .
+```
+
+## Usage
+
+```bash
+solve get boj 1000
+solve run main.cpp
+```
--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name="solve-ps",
+    name="solve",
     version="0.0.0",
-    description="A small cli tool for ps",
+    description="a small cli tool for ps",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=find_packages(),
