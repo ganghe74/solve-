f914cd78099925fac23531c3670d3e0544de3d3c
KH <ganghe74@gmail.com>
2023-02-27 02:30:54 +0900

    setuptools -> poetry

--- solve/main.py
+++ solve/main.py
@@ -1,8 +1,8 @@
 import click
-from src.subs.run import run
-from src.subs.get import get
-from src.subs.diff import diff
-from src.subs.tc import tc
+from solve.subs.run import run
+from solve.subs.get import get
+from solve.subs.diff import diff
+from solve.subs.tc import tc
 
 @click.group()
 def cli():
