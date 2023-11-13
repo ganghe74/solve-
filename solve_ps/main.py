bbbc976036fb84b30cf009972983ebc2f9c16df1
KH <ganghe74@gmail.com>
2023-11-13 11:29:47 +0900

    add --version option

--- solve_ps/main.py
+++ solve_ps/main.py
@@ -5,8 +5,9 @@ from solve_ps.subs.diff import diff
 from solve_ps.subs.tc import tc
 
 @click.group()
+@click.version_option()
 def cli():
-    pass
+    ...
 
 cli.add_command(run)
 cli.add_command(get)
